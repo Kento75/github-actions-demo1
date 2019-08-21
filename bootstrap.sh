ddf9f5b8223f5220415356a923d50eae5bb3fcdd
Kento75 <kento2github@gmail.com>
2019-08-21 17:43:16 +0900

    Added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!bin/sh -ex
+
+echo "Install setup modules ..."
+
+### make base directory
+sudo mkdir /usr/local/
+sudo mkdir /usr/local/bin/
+
+### Adding PATH
+echo 'export PATH=/usr/local:$PATH' >> ~/.bash_profile
+echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bash_profile
+
+### Install Xcode devtools
+xcode-select --install
+
+### Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+source ~/.bash_profile
+brew -v
+brew update
+brew install caskroom/cask/brew-cask
+
+# Install VirtualBox
+brew install virtualbox
+
+# Install Vagrant
+brew install vagrant
+
+
+echo "Setup VM ..."
+
+vagrant ./VMs/provisioning/vagrant up
+vagrant ./VMs/sandbox/vagrant up
